3b1b88649f9f8b209b26f4e020e78cdfab2dadfa
Wiktor Obrębski <psychowico@gmail.com>
2019-03-24 19:59:09 +0100

    Refactor statstics [world-composer]

--- world-composer/src/components/control-panel/Stats.tsx
+++ world-composer/src/components/control-panel/Stats.tsx
@@ -3,14 +3,18 @@ import './Stats.scss';
 import React, { useEffect, useState } from 'react';
 
 import * as MACGrid from '../../data/atmosphere/MACGrid';
-import { Vector, magnitude, add, multiply } from '../../utils/Math';
+import { magnitude } from '../../utils/Math';
 import { connectContext } from '../../context/SimulationContext';
-import { statsEngineFactory, create } from '../../data/engine/StatsEngine';
-// import Particles from '../../data/engine/Particles';
+import {
+    statsEngineFactory,
+    create,
+    collect,
+} from '../../data/engine/StatsEngine';
 import GlobalTimer from '../../utils/Timer';
 
 interface Props {
     grid: MACGrid.MACGridData;
+    pressure: Float32Array;
     particlesCount: number;
 }
 
@@ -19,10 +23,10 @@ const updateStats = statsEngineFactory();
 export const Stats = connectContext(StatsComponent, ({ state }) => ({
     grid: state.simulation.grid,
     particlesCount: state.simulation.particles.count,
+    pressure: state.simulation.artifacts.lastPressureVector,
 }));
 
-// This component waiting for refactor
-function StatsComponent({ grid, particlesCount }: Props) {
+function StatsComponent({ grid, particlesCount, pressure }: Props) {
     const [statsData, setStatsData] = useState(create);
 
     useEffect(
@@ -37,77 +41,26 @@ function StatsComponent({ grid, particlesCount }: Props) {
         [statsData]
     );
 
-    const divVector = MACGrid.divergenceVector(grid);
-    const length = grid.size ** 2;
-    const pressure = 0;
-    let totalVelocity: Vector = [0, 0];
-    let totalDivergence = 0;
-    for (let i = 0; i < grid.size ** 2; i++) {
-        totalVelocity = add(totalVelocity, [
-            grid.field.velX[i],
-            grid.field.velY[i],
-        ]);
-        // pressure += grid.pressureVector[i];
-        totalDivergence += divVector[i];
-    }
-    const avPressure = pressure / length;
-    const avDivergence = totalDivergence / length;
-    const avVelocity = multiply(totalVelocity, 1 / length);
+    const stats = collect(statsData, grid, pressure);
 
-    // const mouseOverCell = round(mouseOver);
-    // const selectedInd = MACGrid.index(grid, mouseOverCell);
-    // const isSolid = grid.solids[selectedInd] === 1;
-    // const clickedInterpolatedVel = isSolid
-    //     ? [0, 0]
-    //     : MACGrid.interpolateVelocity(grid, mouseOver);
-    // const clickedDivergence = divVector[selectedInd];
-    // const ind = MACGrid.index(grid, mouseOverCell);
-
-    // const selectedNodePressure = grid.pressureVector[ind];
-    // const clickedInterpolatedPress = isSolid
-    //     ? NaN
-    //     : MACGrid.interpolatePressure(grid, mouseOver);
     return (
         <div className="stats">
             <dl>
                 <dt>Av. Velocity</dt>
                 <dd>
-                    ({avVelocity[0].toFixed(3)}, {avVelocity[1].toFixed(3)}) [
-                    {magnitude(avVelocity).toFixed(3)}]
+                    ({stats.avVelocity[0].toFixed(3)},{' '}
+                    {stats.avVelocity[1].toFixed(3)}) [
+                    {magnitude(stats.avVelocity).toFixed(3)}]
                 </dd>
                 <dt>Av. Pressure</dt>
-                <dd>{avPressure.toFixed(3)}</dd>
+                <dd>{stats.avPressure.toFixed(3)}</dd>
                 <dt>Av. Divergence</dt>
-                <dd>{avDivergence.toFixed(3)}</dd>
+                <dd>{stats.avDivergence.toFixed(3)}</dd>
                 <dt>Render FPS</dt>
-                <dd>{statsData.fps}</dd>
+                <dd>{stats.fps}</dd>
                 <dt>Particles</dt>
                 <dd>{particlesCount}</dd>
             </dl>
         </div>
     );
 }
-
-// <dt>Selected</dt>
-// <dd>
-//     ({mouseOver[0].toFixed(3)}, {mouseOver[1].toFixed(3)}) ~(
-//     {mouseOverCell[0]}, {mouseOverCell[1]})
-// </dd>
-// <dt>Selected velocity</dt>
-// <dd>
-//     ({grid.field.velX[selectedInd].toFixed(3)},
-//     {grid.field.velY[selectedInd].toFixed(3)})
-// </dd>
-
-// <dt>Clicked interp. velocity</dt>
-// <dd>
-//     ({clickedInterpolatedVel[0].toFixed(3)},
-//     {clickedInterpolatedVel[1].toFixed(3)})
-// </dd>
-// <dt>Clicked divergence</dt>
-// <dd>{clickedDivergence.toFixed(3)}</dd>
-// <dt>Selected pressure</dt>
-// <dd>{selectedNodePressure.toFixed(3)}</dd>
-
-// <dt>Clicked interp. pressure:</dt>
-// <dd>{clickedInterpolatedPress.toFixed(3)}</dd>
--- world-composer/src/data/engine/StatsEngine.ts
+++ world-composer/src/data/engine/StatsEngine.ts
@@ -1,10 +1,51 @@
+import * as MACGrid from '../../data/atmosphere/MACGrid';
+import { Vector, add, multiply } from '../../utils/Math';
+
 export interface StatsData {
     fps: number;
+    avPressure: number;
+    avDivergence: number;
+    avVelocity: Vector;
 }
 
 export function create(): StatsData {
     return {
         fps: 0,
+        avPressure: 0,
+        avDivergence: 0,
+        avVelocity: [0, 0],
+    };
+}
+
+export function collect(
+    statsData: StatsData,
+    grid: MACGrid.MACGridData,
+    gridPressure: Float32Array
+) {
+    const divVector = MACGrid.divergenceVector(grid);
+    const vectorSize = grid.size ** 2;
+
+    let pressure = 0;
+    let totalVelocity: Vector = [0, 0];
+    let totalDivergence = 0;
+
+    for (let i = 0; i < grid.size ** 2; i++) {
+        totalVelocity = add(totalVelocity, [
+            grid.field.velX[i],
+            grid.field.velY[i],
+        ]);
+        pressure += gridPressure[i];
+        totalDivergence += divVector[i];
+    }
+    const avPressure = pressure / vectorSize;
+    const avDivergence = totalDivergence / vectorSize;
+    const avVelocity = multiply(totalVelocity, 1 / vectorSize);
+
+    return {
+        ...statsData,
+        avPressure,
+        avDivergence,
+        avVelocity,
     };
 }
 
