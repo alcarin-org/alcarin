54cc85d07e11b351806dbc3c2df2cba36ec7da60
Wiktor Obrębski <psychowico@gmail.com>
2020-06-01 22:33:02 +0200

    Add triple layout component - basic component for website layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,43 @@
 import * as React from 'react';
+
+import {
+  TripleLayout,
+  MainSection,
+  MainSectionHeader,
+  InfoSection,
+  ActionSection,
+} from './TripleLayout';
 import logo from './logo.svg';
 import './App.css';
 
-interface AppProps {}
-
-function App({}: React.Props<AppProps>) {
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TripleLayout>
+        <ActionSection>Do some actions!</ActionSection>
+        <MainSectionHeader>Simple header</MainSectionHeader>
+        <MainSection>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>
+            a long line of texta long line of texta long line of texta long line
+            of texta long line of texta long line of text
+            a long line of texta long line of texta long line of texta long line
+            of texta long line of texta long line of text
+          </p>
+        </MainSection>
+        <InfoSection>Additional Info</InfoSection>
+      </TripleLayout>
     </div>
   );
 }
---
+++ web/src/TripleLayout.tsx
@@ -0,0 +1,55 @@
+import './TripleLayout.css';
+
+import * as React from 'react';
+
+type LayoutSection = typeof MainSection;
+
+interface TripleLayoutProps {
+  children: React.ReactElement[];
+}
+
+export function TripleLayout({ children }: TripleLayoutProps) {
+  const mainSection = children.find((child) => child.type === MainSection);
+  const actionSection = children.find((child) => child.type === ActionSection);
+  const infoSection = children.find((child) => child.type === InfoSection);
+  const mainSectionHeader = children.find(
+    (child) => child.type === MainSectionHeader
+  );
+
+  return (
+    <div className="triple-layout">
+      {actionSection && (
+        <section className="triple-layout__action">{actionSection}</section>
+      )}
+      <section className="triple-layout__main">
+        <div className="triple-layout__main-content">
+          {mainSectionHeader && (
+            <header className="triple-layout__header">
+              {mainSectionHeader}
+            </header>
+          )}
+          <main>{mainSection}</main>
+        </div>
+      </section>
+      {infoSection && (
+        <section className="triple-layout__info">{infoSection}</section>
+      )}
+    </div>
+  );
+}
+
+export function MainSection({ children }: React.PropsWithChildren<{}>) {
+  return <>{children}</>;
+}
+
+export function ActionSection({ children }: React.PropsWithChildren<{}>) {
+  return <>{children}</>;
+}
+
+export function InfoSection({ children }: React.PropsWithChildren<{}>) {
+  return <>{children}</>;
+}
+
+export function MainSectionHeader({ children }: React.PropsWithChildren<{}>) {
+  return <>{children}</>;
+}
