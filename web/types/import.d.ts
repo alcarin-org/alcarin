1d2f0f96561af5f431904a0ace45574cee83760a
Wiktor Obrębski <psychowico@gmail.com>
2020-06-01 08:52:47 +0200

    Add snowpack based react web frontend skeleton

---
+++ web/types/import.d.ts
@@ -0,0 +1,9 @@
+// ESM-HMR Interface: `import.meta.hot`
+
+interface ImportMeta {
+  // TODO: Import the exact .d.ts files from "esm-hmr"
+  // https://github.com/pikapkg/esm-hmr
+
+  // eslint-disable-next-line
+  hot: any;
+}
