1055073f152e9c150f60f30ea21e8fb071766a4d
Wiktor Obrębski <psychowico@gmail.com>
2020-06-02 23:25:32 +0200

    Setup Bulma and scss

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import App from './App';
-import './index.css';
+import 'public/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
